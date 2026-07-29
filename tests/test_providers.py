1b09ace78bd5d5c6694ef1b1bf2cc11b10393130
Freddy Choudja <freddychoudja@gmail.com>
2026-07-29 13:37:22 +0100

    test: add test-providers.py part 02

--- tests/test_providers.py
+++ tests/test_providers.py
@@ -15,3 +15,20 @@ from kliz.providers.google import GoogleProvider
 from kliz.providers.indexnow import IndexNowProvider
 
 
+@pytest.fixture
+def google_client_mocks() -> Iterator[dict[str, Mock]]:
+    with ExitStack() as stack:
+        credentials_factory = stack.enter_context(
+            patch(
+                "kliz.providers.google.service_account.Credentials."
+                "from_service_account_file"
+            )
+        )
+        http_factory = stack.enter_context(patch("kliz.providers.google.httplib2.Http"))
+        authorized_http_factory = stack.enter_context(
+            patch("kliz.providers.google.google_auth_httplib2.AuthorizedHttp")
+        )
+        build = stack.enter_context(patch("kliz.providers.google.build"))
+        yield {
+            "credentials_factory": credentials_factory,
+            "http_factory": http_factory,
