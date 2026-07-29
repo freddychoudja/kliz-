0de33a27129e606211cc92018686b707ed59e47e
Freddy Choudja <freddychoudja@gmail.com>
2026-07-29 13:37:22 +0100

    test: add test-core.py part 02

--- tests/test_core.py
+++ tests/test_core.py
@@ -15,3 +15,20 @@ class StubProvider(BaseProvider):
     def notify(self, url: str) -> bool:
         return self.callback(url)
 
+
+class NamedProvider(StubProvider):
+    @property
+    def name(self) -> str:
+        return "custom"
+
+
+def return_true(url: str) -> bool:
+    return bool(url)
+
+
+def return_false(url: str) -> bool:
+    return False
+
+
+def raise_retryable(url: str) -> bool:
+    raise ProviderError(
